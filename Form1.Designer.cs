33c5cf365e4d9f2f553da0e6ad39124fbfa18c91
triyak81 <triyak81@gmail.com>
2026-09-07 17:45:25 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace PLCController
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnYes = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblY0Status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnYes
+            // 
+            this.btnYes.Location = new System.Drawing.Point(143, 104);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(133, 57);
+            this.btnYes.TabIndex = 0;
+            this.btnYes.Text = "button1";
+            this.btnYes.UseVisualStyleBackColor = true;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.Location = new System.Drawing.Point(397, 104);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(133, 57);
+            this.btnNo.TabIndex = 0;
+            this.btnNo.Text = "button1";
+            this.btnNo.UseVisualStyleBackColor = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(159, 250);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "label1";
+            // 
+            // lblY0Status
+            // 
+            this.lblY0Status.AutoSize = true;
+            this.lblY0Status.Location = new System.Drawing.Point(310, 250);
+            this.lblY0Status.Name = "lblY0Status";
+            this.lblY0Status.Size = new System.Drawing.Size(35, 13);
+            this.lblY0Status.TabIndex = 1;
+            this.lblY0Status.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblY0Status);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnYes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblY0Status;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace PLCController
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort = new SerialPort("COM1", 9600, Parity.None, 8, StopBits.One);
+                serialPort.Open();
+                lblStatus.Text = "Durum: Bağlı";
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "Hata: " + ex.Message;
+                MessageBox.Show("COM portu açılamadı! " + ex.Message);
+            }
+        }
+
+        // Modbus RTU CRC hesapla
+        private ushort CalculateCRC(byte[] data, int length)
+        {
+            ushort crc = 0xFFFF;
+            for (int i = 0; i < length; i++)
+            {
+                crc ^= data[i];
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((crc & 1) == 1)
+                        crc = (ushort)((crc >> 1) ^ 0xA001);
+                    else
+                        crc = (ushort)(crc >> 1);
+                }
+            }
+            return crc;
+        }
+
+        // Y0'ı AÇ
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] command = new byte[8];
+                command[0] = 0x01;        // Slave ID
+                command[1] = 0x05;        // Function Code
+                command[2] = 0x02;        // Address High Byte
+                command[3] = 0x00;        // Address Low Byte
+                command[4] = 0xFF;        // Value High Byte (ON)
+                command[5] = 0x00;        // Value Low Byte
+
+                ushort crc = CalculateCRC(command, 6);
+                command[6] = (byte)(crc & 0xFF);
+                command[7] = (byte)((crc >> 8) & 0xFF);
+
+                serialPort.Write(command, 0, 8);
+                lblY0Status.Text = "Y0: ON";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+
+        // Y0'ı KAPA
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] command = new byte[8];
+                command[0] = 0x01;        // Slave ID
+                command[1] = 0x05;        // Function Code
+                command[2] = 0x02;        // Address High Byte
+                command[3] = 0x00;        // Address Low Byte
+                command[4] = 0x00;        // Value High Byte (OFF)
+                command[5] = 0x00;        // Value Low Byte
+
+                ushort crc = CalculateCRC(command, 6);
+                command[6] = (byte)(crc & 0xFF);
+                command[7] = (byte)((crc >> 8) & 0xFF);
+
+                serialPort.Write(command, 0, 8);
+                lblY0Status.Text = "Y0: OFF";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+    }
+}
